7f0839a138b41dde8a88f570151a2f29d5819083
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-15 20:42:37 +0200

    [store] store struct

---
+++ store/store.go
@@ -0,0 +1,15 @@
+package store
+
+type StoreOpts struct {
+	pathTransform pathTransformFunc
+}
+
+type Store struct {
+	StoreOpts
+}
+
+func NewStore(opts StoreOpts) *Store {
+	return &Store{
+		StoreOpts: opts,
+	}
+}
