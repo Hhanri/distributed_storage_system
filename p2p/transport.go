00a2b0b837ba57100fa4baac4d6dd30a82b2dc48
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-15 17:10:32 +0200

    [p2p] transport interface

---
+++ p2p/transport.go
@@ -0,0 +1,11 @@
+package p2p
+
+// Peer is any remote note
+type Peer interface {
+}
+
+// Transport is anything that handles communication
+// between the nodes in a network (TCP, UDP, websockets, ...)
+type Transport interface {
+	ListenAndAccept() error
+}
