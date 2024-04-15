3882fb0107ea32c6a6d9e83f36e413dc991de9da
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-15 17:26:22 +0200

    [p2p] decoder receives a message as param instead of any, default decoder

--- p2p/encoding.go
+++ p2p/encoding.go
@@ -6,11 +6,26 @@ import (
 )
 
 type Decoder interface {
-	Decode(reader io.Reader, v any) error
+	Decode(reader io.Reader, msg *Message) error
 }
 
 type GOBDecoder struct{}
 
-func (d GOBDecoder) Decode(reader io.Reader, v any) error {
-	return gob.NewDecoder(reader).Decode(v)
+func (d GOBDecoder) Decode(reader io.Reader, msg *Message) error {
+	return gob.NewDecoder(reader).Decode(msg)
+}
+
+type DefaultDecoder struct{}
+
+func (d DefaultDecoder) Decode(reader io.Reader, msg *Message) error {
+	buff := make([]byte, 1024)
+	n, err := reader.Read(buff)
+
+	if err != nil {
+		return err
+	}
+
+	msg.Payload = buff[:n]
+
+	return nil
 }
