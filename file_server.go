71fe97104021e3f5dd4298ea51134c6eb9d8ec1d
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-16 19:20:05 +0200

    [server] file server loop and stop

--- file_server.go
+++ file_server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Hhanri/distributed_storage_system/p2p"
 	"github.com/Hhanri/distributed_storage_system/store"
 )
@@ -13,16 +15,45 @@ type FileServerOpts struct {
 type FileServer struct {
 	FileServerOpts
 	store *store.Store
+
+	quitCh chan struct{}
 }
 
 func NewFileServer(opts FileServerOpts) *FileServer {
-
 	return &FileServer{
 		FileServerOpts: opts,
 		store:          store.NewStore(opts.StoreOpts),
+		quitCh:         make(chan struct{}),
 	}
 }
 
+func (fs *FileServer) loop() {
+
+	defer func() {
+		fmt.Println("File server shutting down")
+	}()
+
+	for {
+		select {
+		case msg := <-fs.Transport.Consume():
+			fmt.Println(msg)
+		case <-fs.quitCh:
+			return
+		}
+	}
+
+}
+
+func (fs *FileServer) Stop() {
+	close(fs.quitCh)
+}
+
 func (fs *FileServer) Start() error {
-	return fs.Transport.ListenAndAccept()
+	if err := fs.Transport.ListenAndAccept(); err != nil {
+		return err
+	}
+
+	fs.loop()
+
+	return nil
 }
