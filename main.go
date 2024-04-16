10361398543e215f6b82ced07781a3b822d4b7a2
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-16 11:16:57 +0200

    main initiating transport

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/Hhanri/distributed_storage_system/p2p"
+)
 
 func main() {
-	fmt.Println("Distributed Storage System")
+	tcpOpts := p2p.TCPTransportOpts{
+		ListenAddress: ":3000",
+		Handshaker:    p2p.NOPHandshaker{},
+		Decoder:       p2p.DefaultDecoder{},
+	}
+
+	transport := p2p.NewTCPTransport(tcpOpts)
+
+	if err := transport.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
 }
