aaf61b4b787913f925ecd1c5944712b4e03600d5
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-16 15:05:05 +0200

    [store] test path transform fullPath

--- store/path_test.go
+++ store/path_test.go
@@ -7,12 +7,15 @@ func TestPathTransform(t *testing.T) {
 	key := "somepicture"
 	path := hashPathTransform(root, key)
 	expectedPathName := "../storage_content/0d1a9/04d68/8adeb/3eab5/8fa69/46e5d/0a5dd/90b0f"
-	expectedOriginal := "0d1a904d688adeb3eab58fa6946e5d0a5dd90b0f"
-
+	expectedFileName := "0d1a904d688adeb3eab58fa6946e5d0a5dd90b0f"
+	expectedFullPath := expectedPathName + "/" + expectedFileName
 	if path.PathName != expectedPathName {
 		t.Errorf("expected %s\ngot %s", expectedPathName, path.PathName)
 	}
-	if path.FileName != expectedOriginal {
-		t.Errorf("expected %s\ngot %s", expectedOriginal, path.FileName)
+	if path.FileName != expectedFileName {
+		t.Errorf("expected %s\ngot %s", expectedFileName, path.FileName)
+	}
+	if path.FullPath() != expectedFullPath {
+		t.Errorf("expected %s\ngot %s", expectedFullPath, path.FullPath())
 	}
 }
