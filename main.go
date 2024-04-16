f9c678e5b93f7ff246c7dd1afc2f65bc239120c5
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-16 19:07:40 +0200

    [server] main instantiating server

--- main.go
+++ main.go
@@ -4,18 +4,32 @@ import (
 	"log"
 
 	"github.com/Hhanri/distributed_storage_system/p2p"
+	"github.com/Hhanri/distributed_storage_system/store"
 )
 
 func main() {
+
 	tcpOpts := p2p.TCPTransportOpts{
 		ListenAddress: ":3000",
 		Handshaker:    p2p.NOPHandshaker{},
 		Decoder:       p2p.DefaultDecoder{},
 	}
-
 	transport := p2p.NewTCPTransport(tcpOpts)
 
-	if err := transport.ListenAndAccept(); err != nil {
+	fileServerOtps := FileServerOpts{
+		StoreOpts: store.StoreOpts{
+			Root:          store.DefaultRootStorage,
+			PathTransform: store.HashPathTransform,
+		},
+
+		Transport: transport,
+	}
+
+	server := NewFileServer(fileServerOtps)
+
+	if err := server.Start(); err != nil {
 		log.Fatal(err)
 	}
+
+	select {}
 }
